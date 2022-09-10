4554062549293c9407ce2d2540dd819ddbf4bc64
Matthew Murnaghan <matt.murnaghan@outlook.ie>
2022-09-10 19:26:11 +0000

    Add requirements for deployment

--- run.py
+++ run.py
@@ -4,7 +4,6 @@ at a shop and update a google sheet with this data
 """
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -25,7 +24,7 @@ def get_sales_data():
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
         print("Examples: 10,20,30,40,50,60\n")
-        data_str = input("Enter your data here: ")
+        data_str = input("Enter your data here:\n")
         sales_data = data_str.split(',')
         if validate_data(sales_data):
             print("Data is valid")
